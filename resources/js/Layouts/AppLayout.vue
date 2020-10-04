904e9f97d8c2791956119ce41d461b5c37e0c17d
Harun R Rayhan <harun.b13@gmail.com>
2020-10-04 18:30:02 +0600

    👌 IMPROVE: Update Layout Component

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -2,13 +2,13 @@
     <div class="min-h-screen bg-gray-100">
         <nav class="bg-white border-b border-gray-100">
             <!-- Primary Navigation Menu -->
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto px-12 md:px-12 sm:px-6 lg:px-12">
                 <div class="flex justify-between h-16">
                     <div class="flex">
                         <!-- Logo -->
                         <div class="flex-shrink-0 flex items-center">
-                            <a href="/dashboard">
-                                <jet-application-mark class="block h-9 w-auto" />
+                            <a href="/">
+                                <jet-application-mark class="block h-9 w-auto"/>
                             </a>
                         </div>
 
@@ -25,16 +25,22 @@
                         <div class="ml-3 relative">
                             <jet-dropdown align="right" width="48">
                                 <template #trigger>
-                                    <button v-if="$page.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                                        <img class="h-8 w-8 rounded-full object-cover" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
+                                    <button v-if="$page.jetstream.managesProfilePhotos"
+                                            class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                        <img class="h-8 w-8 rounded-full object-cover"
+                                             :src="$page.user.profile_photo_url" :alt="$page.user.name"/>
                                     </button>
 
-                                    <button v-else class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    <button v-else
+                                            class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                         <div>{{ $page.user.name }}</div>
 
                                         <div class="ml-1">
-                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                 viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                      clip-rule="evenodd"/>
                                             </svg>
                                         </div>
                                     </button>
@@ -82,7 +88,13 @@
                                             <form @submit.prevent="switchToTeam(team)">
                                                 <jet-dropdown-link as="button">
                                                     <div class="flex items-center">
-                                                        <svg v-if="team.id == $page.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                        <svg v-if="team.id == $page.user.current_team_id"
+                                                             class="mr-2 h-5 w-5 text-green-400" fill="none"
+                                                             stroke-linecap="round" stroke-linejoin="round"
+                                                             stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                                                            <path
+                                                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                                        </svg>
                                                         <div>{{ team.name }}</div>
                                                     </div>
                                                 </jet-dropdown-link>
@@ -105,10 +117,17 @@
 
                     <!-- Hamburger -->
                     <div class="-mr-2 flex items-center sm:hidden">
-                        <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <button @click="showingNavigationDropdown = ! showingNavigationDropdown"
+                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                <path
+                                    :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }"
+                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h16"/>
+                                <path
+                                    :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
+                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"/>
                             </svg>
                         </button>
                     </div>
@@ -127,7 +146,8 @@
                 <div class="pt-4 pb-1 border-t border-gray-200">
                     <div class="flex items-center px-4">
                         <div class="flex-shrink-0">
-                            <img class="h-10 w-10 rounded-full" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
+                            <img class="h-10 w-10 rounded-full" :src="$page.user.profile_photo_url"
+                                 :alt="$page.user.name"/>
                         </div>
 
                         <div class="ml-3">
@@ -137,11 +157,14 @@
                     </div>
 
                     <div class="mt-3 space-y-1">
-                        <jet-responsive-nav-link href="/user/profile" :active="$page.currentRouteName == 'profile.show'">
+                        <jet-responsive-nav-link href="/user/profile"
+                                                 :active="$page.currentRouteName == 'profile.show'">
                             Profile
                         </jet-responsive-nav-link>
 
-                        <jet-responsive-nav-link href="/user/api-tokens" :active="$page.currentRouteName == 'api-tokens.index'" v-if="$page.jetstream.hasApiFeatures">
+                        <jet-responsive-nav-link href="/user/api-tokens"
+                                                 :active="$page.currentRouteName == 'api-tokens.index'"
+                                                 v-if="$page.jetstream.hasApiFeatures">
                             API Tokens
                         </jet-responsive-nav-link>
 
@@ -161,11 +184,13 @@
                             </div>
 
                             <!-- Team Settings -->
-                            <jet-responsive-nav-link :href="'/teams/' + $page.user.current_team.id" :active="$page.currentRouteName == 'teams.show'">
+                            <jet-responsive-nav-link :href="'/teams/' + $page.user.current_team.id"
+                                                     :active="$page.currentRouteName == 'teams.show'">
                                 Team Settings
                             </jet-responsive-nav-link>
 
-                            <jet-responsive-nav-link href="/teams/create" :active="$page.currentRouteName == 'teams.create'">
+                            <jet-responsive-nav-link href="/teams/create"
+                                                     :active="$page.currentRouteName == 'teams.create'">
                                 Create New Team
                             </jet-responsive-nav-link>
 
@@ -180,7 +205,12 @@
                                 <form @submit.prevent="switchToTeam(team)" :key="team.id">
                                     <jet-responsive-nav-link as="button">
                                         <div class="flex items-center">
-                                            <svg v-if="team.id == $page.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                            <svg v-if="team.id == $page.user.current_team_id"
+                                                 class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round"
+                                                 stroke-linejoin="round" stroke-width="2" stroke="currentColor"
+                                                 viewBox="0 0 24 24">
+                                                <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                            </svg>
                                             <div>{{ team.name }}</div>
                                         </div>
                                     </jet-responsive-nav-link>
@@ -192,16 +222,12 @@
             </div>
         </nav>
 
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <slot name="header"></slot>
-            </div>
-        </header>
-
         <!-- Page Content -->
         <main>
-            <slot></slot>
+            <div class="px-4 py-8 md:p-12 md:overflow-y-auto" scroll-region>
+                <flash-message />
+                <slot></slot>
+            </div>
         </main>
 
         <!-- Modal Portal -->
@@ -211,49 +237,52 @@
 </template>
 
 <script>
-    import JetApplicationLogo from './../Jetstream/ApplicationLogo'
-    import JetApplicationMark from './../Jetstream/ApplicationMark'
-    import JetDropdown from './../Jetstream/Dropdown'
-    import JetDropdownLink from './../Jetstream/DropdownLink'
-    import JetNavLink from './../Jetstream/NavLink'
-    import JetResponsiveNavLink from './../Jetstream/ResponsiveNavLink'
+import JetApplicationLogo from './../Jetstream/ApplicationLogo'
+import JetApplicationMark from './../Jetstream/ApplicationMark'
+import JetDropdown from './../Jetstream/Dropdown'
+import JetDropdownLink from './../Jetstream/DropdownLink'
+import JetNavLink from './../Jetstream/NavLink'
+import JetResponsiveNavLink from './../Jetstream/ResponsiveNavLink'
+import FlashMessage from "../Shared/FlashMessage";
 
-    export default {
-        components: {
-            JetApplicationLogo,
-            JetApplicationMark,
-            JetDropdown,
-            JetDropdownLink,
-            JetNavLink,
-            JetResponsiveNavLink,
+export default {
+    name: "AppLayout",
+    components: {
+        JetApplicationLogo,
+        JetApplicationMark,
+        JetDropdown,
+        JetDropdownLink,
+        JetNavLink,
+        JetResponsiveNavLink,
+        FlashMessage,
+    },
+
+    data() {
+        return {
+            showingNavigationDropdown: false,
+        }
+    },
+
+    methods: {
+        switchToTeam(team) {
+            this.$inertia.put('/current-team', {
+                'team_id': team.id
+            }, {
+                preserveState: false
+            })
         },
 
-        data() {
-            return {
-                showingNavigationDropdown: false,
-            }
+        logout() {
+            axios.post('/logout').then(response => {
+                window.location = '/';
+            })
         },
+    },
 
-        methods: {
-            switchToTeam(team) {
-                this.$inertia.put('/current-team', {
-                    'team_id': team.id
-                }, {
-                    preserveState: false
-                })
-            },
-
-            logout() {
-                axios.post('/logout').then(response => {
-                    window.location = '/';
-                })
-            },
-        },
-
-        computed: {
-            path() {
-                return window.location.pathname
-            }
+    computed: {
+        path() {
+            return window.location.pathname
         }
     }
+}
 </script>
