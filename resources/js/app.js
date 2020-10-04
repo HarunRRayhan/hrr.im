1e9961672da662fca44db82c9634f1646b5eeecd
Harun R Rayhan <harun.b13@gmail.com>
2020-10-04 18:33:28 +0600

    👌 IMPROVE: Modified Inertia Settings

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,25 @@
 require('./bootstrap');
 
 import Vue from 'vue';
-
-import { InertiaApp } from '@inertiajs/inertia-vue';
-import { InertiaForm } from 'laravel-jetstream';
+import {InertiaApp} from '@inertiajs/inertia-vue';
+import VueMeta from 'vue-meta';
+import {InertiaForm} from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import { InertiaProgress } from '@inertiajs/progress/src';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(VueMeta);
+
+InertiaProgress.init();
 
 const app = document.getElementById('app');
 
 new Vue({
+    metaInfo: {
+        titleTemplate: (title) => title ? `${title} - HRR.IM` : 'HRR.IM'
+    },
     render: (h) =>
         h(InertiaApp, {
             props: {
