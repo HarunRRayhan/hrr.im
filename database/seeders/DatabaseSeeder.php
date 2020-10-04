b0cd30261ba95ccc96d7ba795138fe7bb4dd4e4b
Harun R Rayhan <harun.b13@gmail.com>
2020-10-04 19:36:18 +0600

    👌 Update: seed default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory()->create( [
+            'name'  => 'Harun R Rayhan',
+            'email' => 'me@hrr.im',
+        ] );
     }
 }
