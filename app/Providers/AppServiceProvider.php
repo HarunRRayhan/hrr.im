95b4d1d3bb942a804cc973f61f9bce0b22309f36
Harun R Rayhan <harun.b13@gmail.com>
2020-10-04 17:47:17 +0600

    ⚡ ADD: flash/session data share with inertia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
     }
 
     /**
@@ -23,6 +26,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerInertia();
+    }
+
+    public function registerInertia()
+    {
+        Inertia::version( function () {
+            return md5_file( public_path( 'mix-manifest.json' ) );
+        } );
+
+        Inertia::share( [
+            'auth'  => function () {
+                return [
+                    'user' => Auth::user() ? [
+                        'id'    => Auth::user()->id,
+                        'name'  => Auth::user()->name,
+                        'email' => Auth::user()->email,
+                    ] : null,
+                ];
+            },
+            'flash' => function () {
+                return [
+                    'success' => Session::get( 'success' ),
+                    'error'   => Session::get( 'error' ),
+                ];
+            },
+        ] );
     }
 }
