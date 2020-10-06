47a01f658fedc7ef8899d7256851cf75274e0ac2
Harun R Rayhan <harun.b13@gmail.com>
2020-10-06 18:15:17 +0600

    🚧 WIP: Links table page

--- resources/js/Pages/Links/Index.vue
+++ resources/js/Pages/Links/Index.vue
@@ -11,19 +11,21 @@
         <div class="bg-white rounded shadow overflow-x-auto">
             <table class="w-full whitespace-no-wrap">
                 <tr class="text-left font-bold">
-                    <th class="px-6 pt-6 pb-4">Name</th>
-                    <th class="px-6 pt-6 pb-4">Organization</th>
-                    <th class="px-6 pt-6 pb-4">City</th>
-                    <th class="px-6 pt-6 pb-4" colspan="2">Phone</th>
+                    <th class="px-6 pt-6 pb-4">Label</th>
+                    <th class="px-6 pt-6 pb-4">Shortlink</th>
+                    <th class="px-6 pt-6 pb-4">Full URL</th>
+                    <th class="px-6 pt-6 pb-4" colspan="2">Type</th>
                 </tr>
                 <tr
+                    v-for="link in links.data"
+                    :key="link.id"
                     class="hover:bg-gray-100 focus-within:bg-gray-100">
                     <td class="border-t">
                         <inertia-link
                             class="px-6 py-4 flex items-center focus:text-indigo-500"
-                            href="/"
+                            :href="route('profile.show')"
                         >
-                            My Name
+                            {{ link.label | char_limit(36) }}
                             <icon
                                 name="trash"
                                 class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2"
@@ -37,7 +39,7 @@
                             tabindex="-1"
                         >
                             <div>
-                                My Organization
+                                {{ link.shortlink | char_limit(36) }}
                             </div>
                         </inertia-link>
                     </td>
@@ -47,7 +49,7 @@
                             href="/"
                             tabindex="-1"
                         >
-                            City
+                            {{ link.full_link | char_limit(50) }}
                         </inertia-link>
                     </td>
                     <td class="border-t">
@@ -56,7 +58,7 @@
                             href="/"
                             tabindex="-1"
                         >
-                            Phone Number
+                            type
                         </inertia-link>
                     </td>
                     <td class="border-t w-px">
@@ -69,12 +71,12 @@
                         </inertia-link>
                     </td>
                 </tr>
-<!--                <tr v-if="!contacts">-->
-<!--                    <td class="border-t px-6 py-4" colspan="4">No contacts found.</td>-->
-<!--                </tr>-->
+                <tr v-if="!links.data.length">
+                    <td class="border-t px-6 py-4" colspan="4">No shortlink found.</td>
+                </tr>
             </table>
         </div>
-        <!--        <pagination :links="contacts.links"/>-->
+                <pagination :links="links.links"/>
     </div>
 </template>
 
@@ -83,6 +85,7 @@ import AppLayout from '../../Layouts/AppLayout'
 import LinkTable from "../../Shared/LinkTable";
 import SearchFilter from "../../Shared/SearchFilter";
 import Icon from "../../Shared/Icon";
+import Pagination from "../../Shared/Pagination";
 
 export default {
     components: {
@@ -90,12 +93,12 @@ export default {
         LinkTable,
         SearchFilter,
         Icon,
+        Pagination,
     },
     metaInfo: {title: 'Dashboard'},
     layout: AppLayout,
     props: {
-        contacts: Object,
-        filters: Object,
+        links: Object,
     },
     data() {
         return {
