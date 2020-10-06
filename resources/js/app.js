a3465253c3b3847818ce0142834fd3fdf45a01dd
Harun R Rayhan <harun.b13@gmail.com>
2020-10-06 14:21:08 +0600

    👌 Update: app name in title

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import {InertiaApp} from '@inertiajs/inertia-vue';
 import VueMeta from 'vue-meta';
 import {InertiaForm} from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
-import { InertiaProgress } from '@inertiajs/progress/src';
+import {InertiaProgress} from '@inertiajs/progress/src';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
@@ -15,10 +15,11 @@ Vue.use(VueMeta);
 InertiaProgress.init();
 
 const app = document.getElementById('app');
+const appName = process.env.MIX_APP_NAME;
 
 new Vue({
     metaInfo: {
-        titleTemplate: (title) => title ? `${title} - HRR.IM` : 'HRR.IM'
+        titleTemplate: (title) => title ? `${title} - ${appName}` : appName
     },
     render: (h) =>
         h(InertiaApp, {
