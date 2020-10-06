b3122b73655a5555355ac36850fe8bc948e8b7d9
Harun R Rayhan <harun.b13@gmail.com>
2020-10-06 19:32:47 +0600

    👌 Update: public getter

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -16,17 +16,19 @@ use Illuminate\Database\Eloquent\Model;
  * @property string|null $description
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read bool $is_public
+ * @property-read string $shortlink
  * @method static \Illuminate\Database\Eloquent\Builder|Link newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Link newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Link query()
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereCreatedAt( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereDescription( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereFullLink( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereId( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereLabel( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereSecret( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereSlug( $value )
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereUpdatedAt( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereFullLink($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereLabel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Link extends Model
@@ -35,10 +37,15 @@ class Link extends Model
 
     protected $guarded = [];
 
-    protected $appends = [ 'shortlink' ];
+    protected $appends = [ 'shortlink', 'is_public' ];
 
-    public function getShortlinkAttribute()
+    public function getShortlinkAttribute(): string
     {
         return route( 'shortlink', [ 'link' => $this->slug, 'secret' => $this->secret ] );
+    }
+
+    public function getIsPublicAttribute(): bool
+    {
+        return (bool) !$this->secret;
     }
 }
