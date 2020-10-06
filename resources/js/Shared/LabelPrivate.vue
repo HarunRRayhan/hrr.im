d8f6fb8b08cbeee7adee2afc845b6efa994627d8
Harun R Rayhan <harun.b13@gmail.com>
2020-10-06 19:31:43 +0600

    📦 NEW: Table label tags

---
+++ resources/js/Shared/LabelPrivate.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="px-2 py-1 rounded-full w-24 flex items-center justify-center bg-red-100 text-red-500">
+        <icon name="lock-closed" class="block w-3 h-3 fill-current"/>
+        <span class="pl-2 font-bold text-sm">Private</span>
+    </div>
+</template>
+
+<script>
+import Icon from "./Icon";
+
+export default {
+    name: "LabelPrivate",
+    components: {
+        Icon
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Shared/LabelPublic.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="px-2 py-1 rounded-full w-24 flex items-center justify-center bg-green-100 text-green-500">
+        <icon name="public" class="block w-3 h-3 fill-current"/>
+        <span class="pl-2 font-bold text-sm">Public</span>
+    </div>
+</template>
+
+<script>
+import Icon from "./Icon";
+
+export default {
+    name: "LabelPublic",
+    components: {
+        Icon
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
