69b94b601a66ba0c7834aad12dfbf525a5822d27
Harun R Rayhan <harun.b13@gmail.com>
2020-10-06 18:13:15 +0600

    🌱 Added: Link seeder

---
+++ database/seeders/LinkSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Link;
+use Illuminate\Database\Seeder;
+
+class LinkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Link::factory( 25 )->private()->create();
+        Link::factory( 75 )->create();
+    }
+}
