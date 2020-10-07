a7a7e5c5bfc79fa4a6ab83a5b7b550653a5baf5f
Harun R Rayhan <harun.b13@gmail.com>
2020-10-07 17:28:01 +0600

    ♻️Refactor: Link Index Component

--- resources/js/Pages/Links/Index.vue
+++ resources/js/Pages/Links/Index.vue
@@ -8,69 +8,7 @@
                 <span class="hidden md:inline">Shortlink</span>
             </inertia-link>
         </div>
-        <div class="bg-white rounded shadow overflow-x-auto">
-            <table class="w-full whitespace-no-wrap">
-                <tr class="text-left font-bold">
-                    <th class="px-6 pt-6 pb-4">Label</th>
-                    <th class="px-6 pt-6 pb-4">Shortlink</th>
-                    <th class="px-6 pt-6 pb-4">Full URL</th>
-                    <th class="px-6 pt-6 pb-4" colspan="2">Type</th>
-                </tr>
-                <tr
-                    v-for="link in links.data"
-                    :key="link.id"
-                    class="hover:bg-gray-100 focus-within:bg-gray-100">
-                    <td class="border-t">
-                        <inertia-link
-                            class="px-6 py-4 flex items-center focus:text-indigo-500"
-                            :href="route('profile.show')"
-                        >
-                            {{ link.label | char_limit(36) }}
-                            <icon
-                                name="trash"
-                                class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2"
-                            />
-                        </inertia-link>
-                    </td>
-                    <td class="border-t">
-                        <inertia-link
-                            class="px-6 py-4 flex items-center"
-                            href="/"
-                            tabindex="-1"
-                        >
-                            <div>
-                                {{ link.shortlink | char_limit(36) }}
-                            </div>
-                        </inertia-link>
-                    </td>
-                    <td class="border-t">
-                        <inertia-link
-                            class="px-6 py-4 flex items-center"
-                            href="/"
-                            tabindex="-1"
-                        >
-                            {{ link.full_link | char_limit(50) }}
-                        </inertia-link>
-                    </td>
-                    <td class="border-t">
-                        <label-public v-if="link.is_public"/>
-                        <label-private v-else />
-                    </td>
-                    <td class="border-t w-px">
-                        <inertia-link
-                            class="px-4 flex items-center"
-                            href="/"
-                            tabindex="-1"
-                        >
-                            <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400"/>
-                        </inertia-link>
-                    </td>
-                </tr>
-                <tr v-if="!links.data.length">
-                    <td class="border-t px-6 py-4" colspan="4">No shortlink found.</td>
-                </tr>
-            </table>
-        </div>
+        <link-table :links="links.data"/>
         <pagination :links="links.links"/>
     </div>
 </template>
@@ -79,20 +17,14 @@
 import AppLayout from '../../Layouts/AppLayout'
 import LinkTable from "../../Shared/LinkTable";
 import SearchFilter from "../../Shared/SearchFilter";
-import Icon from "../../Shared/Icon";
 import Pagination from "../../Shared/Pagination";
-import LabelPublic from "../../Shared/LabelPublic";
-import LabelPrivate from "../../Shared/LabelPrivate";
 
 export default {
     components: {
         AppLayout,
         LinkTable,
         SearchFilter,
-        Icon,
         Pagination,
-        LabelPublic,
-        LabelPrivate,
     },
     metaInfo: {title: 'Dashboard'},
     layout: AppLayout,
@@ -103,7 +35,6 @@ export default {
         return {
             form: {
                 search: '',
-                trashed: '',
             },
         }
     },
