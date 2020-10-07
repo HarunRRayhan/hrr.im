011c6409ab3b08cd23369a96c5df5efae603f66e
Harun R Rayhan <harun.b13@gmail.com>
2020-10-07 17:28:49 +0600

    📦 NEW: added link open & copy components

---
+++ resources/js/Shared/Pertials/LinkCopy.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="relative flex items-center">
+        <input class="flex-1 py-2 px-3 rounded border border-indigo-400 focus:outline-none" type="text" :value="link">
+        <button
+            class="absolute flex items-center px-2 right-0 top-0 bottom-0 bg-gray-200 hover:bg-indigo-200 text-gray-500 hover:text-indigo-700 rounded-tr rounded-br border-t border-b border-r border-indigo-400 group text-sm font-bold focus:outline-none"
+            @click="doCopy"
+            v-tooltip.right="{content: toolTipText, show: showToolTip, classes: tooltipClasses, autoHide: false}"
+        >
+            <span class="hidden group-hover:block pr-1">Copy</span>
+            <icon name="copy" class="block w-5 h-5 fill-current"/>
+        </button>
+    </div>
+</template>
+
+<script>
+import Icon from "../Icon";
+
+export default {
+    name: "LinkCopy",
+    components: {
+        Icon,
+    },
+    props: {
+        link: String
+    },
+    data() {
+        return {
+            showToolTip: false,
+            toolTipText: '',
+            copySuccess: true,
+            timeout: null,
+        }
+    },
+    computed: {
+        tooltipClasses() {
+            if (!this.copySuccess) {
+                return 'error';
+            }
+            return 'success';
+        }
+    },
+    methods: {
+        doCopy: function () {
+            const self = this;
+            this.$copyText(this.link).then(function (e) {
+                self.copiedTextMessage(e);
+            }, function (e) {
+                self.copyFailedMessage(e);
+            })
+        },
+        copiedTextMessage(e = null) {
+            this.toolTipText = 'Link Copied';
+            this.copySuccess = true;
+            this.showToolTip = true;
+            this.removeToolTipTimeout();
+            // alert('Copied')
+            console.log(e)
+        },
+        copyFailedMessage(e = null) {
+            this.toolTipText = 'Failed to copy';
+            this.copySuccess = false;
+            this.showToolTip = true;
+            // alert('Can not copy')
+            console.log(e)
+        },
+        removeToolTipTimeout() {
+            const self = this;
+            this.timeout = setTimeout(function () {
+                self.removeToolTip();
+            }, 2000)
+        },
+        removeToolTip() {
+            this.showToolTip = false;
+            this.toolTipText = '';
+            this.copySuccess = true;
+        }
+    },
+    beforeDestroy() {
+        this.timeout = null;
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Shared/Pertials/LinkOpen.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="relative flex items-center">
+        <input class="flex-1 py-2 px-3 rounded border border-gray-300 focus:outline-none" type="text" :value="link">
+        <a
+            class="absolute flex items-center px-2 right-0 top-0 bottom-0 bg-gray-200 hover:bg-green-50 text-gray-500 hover:text-green-400 rounded-tr rounded-br border-t border-b border-r border-gray-300 group text-sm font-bold focus:outline-none"
+            target="_blank"
+            :href="link"
+        >
+            <span class="hidden group-hover:block pr-1">Open</span>
+            <icon name="link-open" class="block w-5 h-5 stroke-current"/>
+        </a>
+    </div>
+</template>
+
+<script>
+import Icon from "../Icon";
+
+export default {
+    name: "LinkOpen",
+    components: {
+        Icon
+    },
+    props: {
+        link: String,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
