6faeefb8f2b6d2766a77f68b8fb11381157c4654
Harun R Rayhan <harun.b13@gmail.com>
2020-10-07 17:26:23 +0600

    👌 Update: added tooltip & clipboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,16 @@ import Vue from 'vue';
 import {InertiaApp} from '@inertiajs/inertia-vue';
 import VueMeta from 'vue-meta';
 import {InertiaForm} from 'laravel-jetstream';
+import VueClipboard from "vue-clipboard2";
+import VTooltip from 'v-tooltip'
 import PortalVue from 'portal-vue';
 import {InertiaProgress} from '@inertiajs/progress';
 import './Mixins/filters';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
+Vue.use(VueClipboard);
+Vue.use(VTooltip);
 Vue.use(PortalVue);
 Vue.use(VueMeta);
 Vue.mixin({methods: {route: window.route}})
