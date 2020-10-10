15139c80dfac1f22afcbb4e1f890752d89943806
Harun R Rayhan <harun.b13@gmail.com>
2020-10-10 18:34:05 +0600

    ✅ TEST: update link tests

--- tests/Feature/Link/EditTest.php
+++ tests/Feature/Link/EditTest.php
@@ -6,6 +6,7 @@ use App\Models\Link;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class EditTest extends TestCase
@@ -66,7 +67,145 @@ class EditTest extends TestCase
              ->assertSessionHasNoErrors();
 
         $linkUpdated = $link->fresh();
-        dump( $linkUpdated );
         $this->assertSame( $link->label, $linkUpdated->label );
+    }
+
+    /** @test */
+    public function it_should_update_slug()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'slug' => 'my-updated-slug',
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $link->refresh();
+        $this->assertSame( 'my-updated-slug', $link->slug );
+    }
+
+    /** @test */
+    public function it_should_not_update_empty_slug()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'slug' => '',
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $linkUpdated = $link->fresh();
+        $this->assertSame( $link->slug, $linkUpdated->slug );
+    }
+
+    /** @test */
+    public function it_should_update_full_link()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'full_link' => 'example.com/my-updated-link',
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $link->refresh();
+        $this->assertSame( 'http://example.com/my-updated-link', $link->full_link );
+    }
+
+    /** @test */
+    public function it_should_not_update_empty_full_link()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'full_link' => '',
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $linkUpdated = $link->fresh();
+        $this->assertSame( $link->full_link, $linkUpdated->full_link );
+    }
+
+    /** @test */
+    public function it_should_update_description()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'description' => 'this is my updated description',
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $link->refresh();
+        $this->assertSame( 'this is my updated description', $link->description );
+    }
+
+    /** @test */
+    public function it_should_remove_description_if_null()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'description' => '',
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $linkUpdated = $link->fresh();
+        $this->assertNull( $linkUpdated->description );
+    }
+
+    /** @test */
+    public function it_can_make_public_to_private()
+    {
+        $link = Link::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'private' => true,
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $linkUpdated = $link->fresh();
+        $this->assertNull( $link->secret );
+        $this->assertNotNull( $linkUpdated->secret );
+        $this->assertTrue( 6 === Str::length( $linkUpdated->secret ) );
+    }
+
+    /** @test */
+    public function it_can_make_private_to_public()
+    {
+        $link = Link::factory()->private()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'private' => false,
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $linkUpdated = $link->fresh();
+        $this->assertNotNull( $link->secret );
+        $this->assertTrue( 6 === Str::length( $link->secret ) );
+        $this->assertNull( $linkUpdated->secret );
+    }
+
+    /** @test */
+    public function it_can_update_with_the_same_slug()
+    {
+        $link = Link::factory()->private()->create();
+        $user = User::factory()->create();
+        $this->actingAs( $user )
+             ->put( route( 'links.update', $link ), [
+                 'slug' => $link->slug
+             ] )
+             ->assertSessionHasNoErrors();
+
+        $linkUpdated = $link->fresh();
+        $this->assertSame( $link->slug, $linkUpdated->slug );
     }
 }
