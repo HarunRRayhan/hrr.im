b815c9eb1e0c8344afdf57d28525526eab6ad920
Harun R Rayhan <harun.b13@gmail.com>
2020-10-10 19:06:45 +0600

    🚧 WIP: show links page

---
+++ resources/js/Pages/Links/Show.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1 class="mb-8 font-bold text-3xl">
+            <inertia-link class="text-indigo-400 hover:text-indigo-600" :href="route('dashboard')">Links
+            </inertia-link>
+            <span class="text-indigo-400 font-medium">/</span> Stats
+        </h1>
+
+
+    </div>
+</template>
+
+<script>
+import AppLayout from "../../Layouts/AppLayout";
+
+export default {
+    name: "Show",
+    metaInfo: {title: 'Create Link'},
+    layout: AppLayout,
+    props: {
+        errors: Object,
+        link: Object,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
