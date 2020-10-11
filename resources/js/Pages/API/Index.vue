e69b49971c0b24bac09b040c12ffda58bde92fd4
Harun R Rayhan <harun.b13@gmail.com>
2020-10-11 17:46:19 +0600

    ♻️Refactor: profile

--- resources/js/Pages/API/Index.vue
+++ resources/js/Pages/API/Index.vue
@@ -1,37 +1,35 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                API Tokens
-            </h2>
-        </template>
-
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <api-token-manager :tokens="tokens"
-                                   :available-permissions="availablePermissions"
-                                   :default-permissions="defaultPermissions" />
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <h1 class="mb-12 font-bold text-3xl">API Tokens</h1>
+                <api-token-manager
+                    :tokens="tokens"
+                    :available-permissions="availablePermissions"
+                    :default-permissions="defaultPermissions"
+                />
             </div>
         </div>
-    </app-layout>
 </template>
 
 <script>
-    import ApiTokenManager from './ApiTokenManager'
-    import AppLayout from './../../Layouts/AppLayout'
-    import JetSectionBorder from './../../Jetstream/SectionBorder'
+import ApiTokenManager from './ApiTokenManager'
+import AppLayout from './../../Layouts/AppLayout'
+import JetSectionBorder from './../../Jetstream/SectionBorder'
 
-    export default {
-        props: [
-            'tokens',
-            'availablePermissions',
-            'defaultPermissions',
-        ],
+export default {
+    props: [
+        'tokens',
+        'availablePermissions',
+        'defaultPermissions',
+    ],
 
-        components: {
-            ApiTokenManager,
-            AppLayout,
-            JetSectionBorder,
-        },
-    }
+    metaInfo: {title: 'API Tokens'},
+    layout: AppLayout,
+
+    components: {
+        ApiTokenManager,
+        AppLayout,
+        JetSectionBorder,
+    },
+}
 </script>
