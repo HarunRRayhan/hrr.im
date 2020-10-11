bd2ccf228db202c3d1f1a73747b15a9fa3826cb7
Harun R Rayhan <harun.b13@gmail.com>
2020-10-11 16:57:55 +0600

    📦 NEW: added stats table

--- resources/js/Pages/Links/Show.vue
+++ resources/js/Pages/Links/Show.vue
@@ -30,32 +30,71 @@
 
                     <div class="mt-4" v-if="link.description">
                         <h5 class="pb-1">Description:</h5>
-                        <div class="" v-html="description"></div>
+                        <div class="bg-indigo-100 rounded px-2 py-2" v-html="description"></div>
                     </div>
+                </div>
+
+                <div
+                    class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex items-center justify-between"
+                >
+                    <button
+                        class="text-red-600 hover:underline"
+                        tabindex="-1"
+                        type="button"
+                        @click="confirmingDelete(link)"
+                    >Delete Contact
+                    </button>
+
+                    <inertia-link
+                        class="btn-indigo"
+                        :href="route('links.edit', link)"
+                        tabindex="-1"
+                    >
+                        Edit Contact
+                    </inertia-link>
                 </div>
             </div>
             <div class="flex-1 bg-white rounded shadow overflow-hidden">
-                <link-prevue :url="link.full_link" cardWidth="100%" :showButton="false">
+                <link-prevue
+                    :url="link.full_link"
+                    cardWidth="100%"
+                    :showButton="false"
+                >
                 </link-prevue>
             </div>
         </div>
 
+        <stats-table
+            :link="link"
+            :statistics="statistics"
+        />
+
+        <delete-modal
+            :show="deleting.confirming"
+            :link="deleting.link"
+            @close="dismissDeleteModal"
+        />
     </div>
 </template>
 
 <script>
 import LinkPrevue from 'link-prevue';
 import AppLayout from "../../Layouts/AppLayout";
+import DeleteModal from "../../Shared/Pertials/Links/DeleteModal";
+import StatsTable from "../../Shared/Pertials/Links/StatsTable";
 import Copy from "../../Shared/Pertials/Links/Copy";
 import Open from "../../Shared/Pertials/Links/Open";
 import Public from "../../Shared/Components/Labels/Public";
 import Private from "../../Shared/Components/Labels/Private";
 import {nl2br} from "../../helpers";
+import deleteLink from "../../Mixins/deleteLink";
 
 export default {
     name: "Show",
     components: {
         LinkPrevue,
+        DeleteModal,
+        StatsTable,
         Copy,
         Open,
         Public,
@@ -63,9 +102,11 @@ export default {
     },
     metaInfo: {title: 'Link Statistics'},
     layout: AppLayout,
+    mixins: [deleteLink],
     props: {
         errors: Object,
         link: Object,
+        statistics: Object,
     },
     computed: {
         description() {
---
+++ resources/js/Shared/Pertials/Links/StatsTable.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="w-full mt-12">
+        <div class="w-full flex items-center mb-4 px-2">
+            <h3 class="font-bold text-2xl">Statistics</h3>
+            <span
+                class="ml-2 text-sm font-bold bg-indigo-200 px-3 py-1 rounded-full text-indigo-700">{{
+                    clicks
+                }} Clicks</span>
+        </div>
+
+
+        <div
+            class="bg-white rounded shadow overflow-x-auto"
+            v-if="statistics.data">
+            <table class="w-full whitespace-no-wrap">
+                <tr class="text-left font-bold">
+                    <th class="px-6 pt-6 pb-4">Browser</th>
+                    <th class="px-6 pt-6 pb-4">Device</th>
+                    <th class="px-6 pt-6 pb-4">OS</th>
+                    <th class="px-6 pt-6 pb-4">Status</th>
+                    <th class="px-6 pt-6 pb-4">IP Address</th>
+                    <th class="px-6 pt-6 pb-4">Referrer</th>
+                </tr>
+                <tr
+                    v-for="stat in statistics.data"
+                    :key="stat.id"
+                    class="hover:bg-gray-100 focus-within:bg-gray-100"
+                >
+                    <td class="border-t">
+                        <span
+                            class="flex px-6 py-4"
+                            v-if="stat.agent.browser.name"
+                        >
+                            <span class="flex items-center">
+                                <icon
+                                    :name="browserIcon(stat.agent.browser.name)"
+                                    class="block w-4 h-4 fill-gray-600 mr-1"
+                                />
+                                {{ stat.agent.browser.name }}
+                            </span>
+                            <span
+                                v-if="stat.agent.browser.version"
+                                class="text-xs font-bold bg-green-100 px-3 py-1 rounded-full text-green-600 flex-end"
+                            >{{ stat.agent.browser.version }}</span>
+                        </span>
+                    </td>
+                    <td class="border-t">
+                        <span class="block px-6 py-4">
+                        {{ deviceName(stat.agent.device) }}
+                        </span>
+                    </td>
+                    <td class="border-t">
+                        <span
+                            class="block px-6 py-4"
+                            v-if="stat.agent.platform.name "
+                        >
+                            <span>{{ stat.agent.platform.name }}</span>
+
+                            <span
+                                v-if="stat.agent.platform.version"
+                                class="text-xs font-bold bg-red-50 px-3 py-1 rounded-full text-red-500"
+                            >{{ stat.agent.platform.version | underscore2dot }}</span>
+                        </span>
+                    </td>
+                    <td class="border-t">
+                        <span
+                            class="block pl-4 pr-6 py-4"
+                        >
+                            <span
+                                v-if="stat.success"
+                                class="text-sm font-bold bg-green-100 px-3 py-1 rounded-full text-green-600"
+                            >Success</span>
+                            <span
+                                v-else
+                                class="text-sm font-bold bg-red-100 px-3 py-1 rounded-full text-red-600"
+                            >Failed</span>
+                        </span>
+                    </td>
+                    <td class="border-t">
+                        <span class="block px-6 py-4">
+                        {{ stat.ip_address }}
+                        </span>
+                    </td>
+                    <td class="border-t">
+                        <span class="block px-6 py-4">
+                        {{ stat.referer }}
+                        </span>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <pagination
+
+            :links="statistics.links"
+        />
+    </div>
+</template>
+
+<script>
+import Pagination from "../../Pagination";
+import Icon from "../../Icon";
+
+export default {
+    name: "StatsTable",
+    components: {
+        Pagination,
+        Icon,
+    },
+    props: {
+        link: Object,
+        statistics: Object,
+    },
+    computed: {
+        clicks() {
+            return this.link.statistics_count ? this.link.statistics_count : 0;
+        },
+    },
+    methods: {
+        deviceName(device) {
+            if (device.name) {
+                return device.name;
+            } else if ('robot' === device.type) {
+                return 'Robot';
+            }
+
+            return "Unknown";
+        },
+        browserIcon(name) {
+            name = name.toLowerCase();
+
+            const browsers = [
+                'chrome',
+                'safari',
+                'opera',
+                'edge',
+            ]
+
+            for (let i = 0; i < browsers.length; i++) {
+                if (name.includes(browsers[i])) return browsers[i];
+            }
+
+            return 'unknown-browser';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
