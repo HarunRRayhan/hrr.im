ed474eccbc1f445ffb33e004d5263289b42ee180
Harun R Rayhan <harun.b13@gmail.com>
2020-10-11 16:56:46 +0600

    📦 NEW: added underscore2dot

--- resources/js/Mixins/filters.js
+++ resources/js/Mixins/filters.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
-import {char_limit, nl2br} from "../helpers";
+import {char_limit, nl2br, underscore2dot} from "../helpers";
 
 Vue.filter('char_limit', char_limit);
 
 Vue.filter('nl2br', nl2br);
+
+Vue.filter('underscore2dot', underscore2dot);
--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -9,14 +9,16 @@ export function char_limit(value, size) {
 }
 
 export function nl2br(str, isXhtml = false) {
-    // Some latest browsers when str is null return and unexpected null value
     if (typeof str === 'undefined' || str === null) {
         return ''
     }
 
-    // Adjust comment to avoid issue on locutus.io display
-    var breakTag = (isXhtml || typeof isXhtml === 'undefined') ? '<br ' + '/>' : '<br>'
+    let breakTag = (isXhtml || typeof isXhtml === 'undefined') ? '<br ' + '/>' : '<br>'
 
     return (str + '')
         .replace(/(\r\n|\n\r|\r|\n)/g, breakTag + '$1')
+}
+
+export function underscore2dot(str) {
+    return str.replaceAll('_', '.');
 }
