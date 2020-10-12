05f29cf112826c46f304dbcfe37fd9984b1e161d
Harun R Rayhan <harun.b13@gmail.com>
2020-10-12 17:45:06 +0600

    👌 Update: default domain to localhost

--- config/app.php
+++ config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     'url'    => env( 'APP_URL', 'http://localhost' ),
-    'domain' => env( 'APP_DOMAIN', 'dash.hrr.im' ),
+    'domain' => env( 'APP_DOMAIN', 'http://dash.localhost' ),
 
     'asset_url' => env( 'ASSET_URL', null ),
 
